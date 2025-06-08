c506484f78c2c86330dc13c825205180d32eddaf
Pettinelli Nicolas <niadpe@gmail.com>
2025-06-07 21:45:52 -0300

    cambiando response, service y controller de persona, tambien creando el mapper de persona

--- src/main/java/ProyectoFinalTienda/TiendaVideojuegos/mappers/PersonaMapper.java
+++ src/main/java/ProyectoFinalTienda/TiendaVideojuegos/mappers/PersonaMapper.java
@@ -1,5 +1,6 @@
 package ProyectoFinalTienda.TiendaVideojuegos.mappers;
 
+import ProyectoFinalTienda.TiendaVideojuegos.dtos.requests.PersonaCreateOrReplaceRequest;
 import ProyectoFinalTienda.TiendaVideojuegos.dtos.responses.PersonaResponse;
 import ProyectoFinalTienda.TiendaVideojuegos.model.entities.PersonaEntity;
 import org.springframework.stereotype.Component;
@@ -7,13 +8,24 @@ import org.springframework.stereotype.Component;
 @Component
 public class PersonaMapper {
 
-    public PersonaResponse toResponse(PersonaEntity entity) {
+    public PersonaEntity convertirDTOaEntidad(PersonaCreateOrReplaceRequest dto) {
+        return PersonaEntity.builder()
+                .nombre(dto.getNombre())
+                .apellido(dto.getApellido())
+                .dni(dto.getDni())
+                .email(dto.getEmail())
+                .telefono(dto.getTelefono())
+                .build();
+    }
+
+
+    public PersonaResponse convertirEntidadADTO(PersonaEntity entity) {
         return PersonaResponse.builder()
-                .personaId(entity.getPersonaId())
                 .nombre(entity.getNombre())
                 .apellido(entity.getApellido())
                 .dni(entity.getDni())
                 .email(entity.getEmail())
+                .telefono(entity.getTelefono())
                 .build();
     }
 
