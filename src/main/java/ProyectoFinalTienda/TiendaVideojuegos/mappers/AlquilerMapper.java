78073b10f2f00f20923cfd901a9485b63f3288b4
Kevin <kevinsk8.1998@gmail.com>
2025-06-12 23:34:20 -0300

    proceso de corregir alquiler

--- src/main/java/ProyectoFinalTienda/TiendaVideojuegos/mappers/AlquilerMapper.java
+++ src/main/java/ProyectoFinalTienda/TiendaVideojuegos/mappers/AlquilerMapper.java
@@ -1,8 +1,10 @@
 package ProyectoFinalTienda.TiendaVideojuegos.mappers;
 
+import ProyectoFinalTienda.TiendaVideojuegos.dtos.requests.AlquilerCreateOrReplaceRequest;
 import ProyectoFinalTienda.TiendaVideojuegos.dtos.responses.AlquilerResponse;
 import ProyectoFinalTienda.TiendaVideojuegos.dtos.responses.PersonaResponse;
 import ProyectoFinalTienda.TiendaVideojuegos.model.entities.AlquilerEntity;
+import ProyectoFinalTienda.TiendaVideojuegos.model.entities.PersonaEntity;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
@@ -14,6 +16,14 @@ public class AlquilerMapper {
     DetalleAlquilerMapper detalleAlquilerMapper;
     @Autowired
     PersonaMapper personaMapper;
+
+    public AlquilerEntity toEntity(AlquilerCreateOrReplaceRequest request, PersonaEntity persona) {
+        return AlquilerEntity.builder()
+                .persona(persona)
+                .fecha_retiro(request.getFecha_retiro())
+                .fecha_devolucion(request.getFecha_devolucion())
+                .build();
+    }
 
     public AlquilerResponse toResponse(AlquilerEntity entity) {
         return AlquilerResponse.builder()
