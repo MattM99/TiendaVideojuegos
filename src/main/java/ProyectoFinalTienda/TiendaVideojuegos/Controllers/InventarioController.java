d38f838e3b5d81725f129b7290d6f62495af379e
Matias <matiasfmendoza21@gmail.com>
2025-06-13 03:27:27 -0300

    - Agregadas más etiquetas de OpenApi

--- src/main/java/ProyectoFinalTienda/TiendaVideojuegos/Controllers/InventarioController.java
+++ src/main/java/ProyectoFinalTienda/TiendaVideojuegos/Controllers/InventarioController.java
@@ -6,6 +6,8 @@ import ProyectoFinalTienda.TiendaVideojuegos.dtos.responses.InventarioResponse;
 import ProyectoFinalTienda.TiendaVideojuegos.model.entities.InventarioEntity;
 import ProyectoFinalTienda.TiendaVideojuegos.model.enums.Plataformas;
 import ProyectoFinalTienda.TiendaVideojuegos.services.InventarioService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -18,57 +20,58 @@ import java.util.List;
 @RestController
 @RequestMapping("/api/inventario")
 @Validated
+@Tag(name = "Inventario", description = "Operaciones relacionadas con la gestión del inventario")
 public class InventarioController {
 
     @Autowired
     private InventarioService inventarioService;
 
-    // Creación de un nuevo inventario
-    @PostMapping("/inventario/crear")
+    @Operation(summary = "Crear un inventario", description = "Crea una nuevo inventario")
+    @PostMapping("/crear")
     public ResponseEntity<InventarioResponse> crearInventario(@Valid @RequestBody InventarioCreateOrReplaceRequest request) {
         InventarioResponse response = inventarioService.guardar(request);
         return ResponseEntity.status(HttpStatus.CREATED).body(response);
     }
 
-    // Eliminar inventario
+    @Operation(summary = "Eliminar un inventario", description = "Elimina un inventario por su ID")
     @DeleteMapping("/eliminar/{id}")
     public ResponseEntity<Void> eliminarInventario(@PathVariable int id) {
         inventarioService.eliminar(id);
         return ResponseEntity.noContent().build();
     }
 
-    // Obtener todos los inventarios
+    @Operation(summary = "Listar todos los inventarios", description = "Devuelve una lista de todos los inventarios")
     @GetMapping("/listar")
     public ResponseEntity<List<InventarioResponse>> listarTodos() {
         return ResponseEntity.ok(inventarioService.obtenerTodos());
     }
 
-    // Obtener inventario por ID
+    @Operation(summary = "Obtener un inventario por ID", description = "Devuelve un inventario específico por su ID")
     @GetMapping("/{id}")
     public ResponseEntity<InventarioResponse> obtenerPorId(@PathVariable int id) {
         InventarioResponse response = inventarioService.buscarPorId(id);
         return  ResponseEntity.ok(response);
     }
 
-    // Buscar por videojuego
+    @Operation(summary = "Buscar inventarios por videojuego", description = "Devuelve una lista de inventarios que contienen un videojuego específico")
     @GetMapping("/videojuego/{videojuegoId}")
     public ResponseEntity<List<InventarioResponse>> buscarPorVideojuego(@PathVariable int videojuegoId) {
         return ResponseEntity.ok(inventarioService.buscarPorVideojuego(videojuegoId));
     }
 
-    // Buscar por plataforma
+    @Operation(summary = "Buscar inventarios por plataforma", description = "Devuelve una lista de inventarios que pertenecen a una plataforma específica")
     @GetMapping("/plataforma/{plataforma}")
     public ResponseEntity<List<InventarioResponse>> buscarPorPlataforma(@PathVariable String plataforma) {
         return ResponseEntity.ok(inventarioService.buscarPorPlataforma(plataforma));
     }
 
-    // Buscar más baratos que cierto valor
+    @Operation(summary = "Buscar inventarios con precio menor a un valor", description = "Devuelve una lista de inventarios cuyo precio es menor al valor especificado")
     @GetMapping("/precio/menor-a")
     public ResponseEntity<List<InventarioResponse>> buscarMasBaratosQue(@RequestParam double valor) {
         return ResponseEntity.ok(inventarioService.buscarMasBaratosQue(valor));
     }
 
-    // Buscar por plataforma y precio menor a cierto valor
+    @Operation(summary = "Buscar inventarios por plataforma y precio menor a un valor", description = "Devuelve una lista de inventarios que pertenecen a una plataforma específica y cuyo precio es menor al valor especificado")
     @GetMapping("/plataformaPrecio/menor-a")
     public ResponseEntity<List<InventarioResponse>> buscarPorPlataformaMasBaratosQue(
             @RequestParam String plataforma,
@@ -76,31 +79,31 @@ public class InventarioController {
         return ResponseEntity.ok(inventarioService.buscarPorPlataformaMasBaratosQue(plataforma, valor));
     }
 
-    // Obtener stock total
+    @Operation(summary = "Obtener stock total", description = "Devuelve el stock total de un inventario por ID")
     @GetMapping("/{id}/stock-total")
     public ResponseEntity<Integer> obtenerStockTotal(@PathVariable int id) {
         return ResponseEntity.ok(inventarioService.obtenerStockTotal(id));
     }
 
-    // Obtener stock disponible
+    @Operation(summary = "Obtener stock disponible", description = "Devuelve el stock disponible de un inventario por ID")
     @GetMapping("/{id}/stock-disponible")
     public ResponseEntity<Integer> obtenerStockDisponible(@PathVariable int id) {
         return ResponseEntity.ok(inventarioService.obtenerStockDisponible(id));
     }
 
-    // Obtener stock alquilado
+    @Operation(summary = "Obtener stock alquilado", description = "Devuelve el stock alquilado de un inventario por ID")
     @GetMapping("/{id}/stock-alquilado")
     public ResponseEntity<Integer> obtenerStockAlquilado(@PathVariable int id) {
         return ResponseEntity.ok(inventarioService.obtenerStockAlquilado(id));
     }
 
-    // Obtener stock descartado
+    @Operation(summary = "Obtener stock descartado", description = "Devuelve el stock descartado de un inventario por ID")
     @GetMapping("/{id}/stock-descartado")
     public ResponseEntity<Integer> obtenerStockDescartado(@PathVariable int id) {
         return ResponseEntity.ok(inventarioService.obtenerStockDescartado(id));
     }
 
-    // PUT: Actualización completa
+    @Operation(summary = "Actualizar inventario completo", description = "Actualiza todos los campos de un inventario por ID")
     @PutMapping("/{id}")
     public ResponseEntity<InventarioResponse> actualizarInventarioCompleto(
             @PathVariable int id,
@@ -109,7 +112,7 @@ public class InventarioController {
         return ResponseEntity.ok(response);
     }
 
-    // PATCH: Actualización parcial
+    @Operation(summary = "Actualizar inventario por campo", description = "Actualiza un campo específico de un inventario por ID")
     @PatchMapping("/{id}")
     public ResponseEntity<InventarioResponse> actualizarInventarioPorCampo(
             @PathVariable int id,
