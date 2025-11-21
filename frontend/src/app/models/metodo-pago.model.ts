b371cb1dde44803a46a2f5faf701937f6fd276b4
KevPF98 <kevinsk8.1998@gmail.com>
2025-11-21 05:31:21 -0300

    Agrego modelo pago

--- frontend/src/app/models/metodo-pago.model.ts
+++ frontend/src/app/models/metodo-pago.model.ts
@@ -7,3 +7,12 @@ export type MetodoPagoUnion = MetodoPagoEfectivoModel | MetodoPagoTarjetaModel;
 export interface MetodoPagoModel {
     tipo: string; //  'EFECTIVO' | 'TARJETA' discriminador para Angular
 }
+
+
+/* EJEMPLO DE COMO SE TRATARÍA EL MÉTODO DE PAGO USANDO EL MODELO UNIÓN:
+if (pago.metodoPago.tipo === 'EFECTIVO') {
+  // nada que hacer, siempre true
+} else if (pago.metodoPago.tipo === 'TARJETA') {
+  const tarjeta = pago.metodoPago as MetodoPagoTarjeta;
+  // usar tarjeta.numero, titular, etc.
+}*/
