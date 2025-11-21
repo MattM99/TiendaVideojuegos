04522bd3f1ddda6d0b2e3cd0a7c0218f5ecc22a0
KevPF98 <kevinsk8.1998@gmail.com>
2025-11-21 04:23:33 -0300

    Agrego modelo Persona

--- frontend/src/app/models/persona.model.ts
+++ frontend/src/app/models/persona.model.ts
@@ -1,2 +1,8 @@
 export interface PersonaModel {
+    id?: number; //Lo dejo opcional para cuando se cree una nueva persona
+    dni: string;
+    nombre: string;
+    apellido: string;
+    email?: string; //Opcional
+    telefono?: string; //Opcional
 }
