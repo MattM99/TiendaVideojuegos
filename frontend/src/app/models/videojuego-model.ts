1e1954c02da1dcd95713c91fc9140de356e99848
KevPF98 <kevinsk8.1998@gmail.com>
2025-11-21 03:48:36 -0300

    Agrego modelo Videojuego

---
+++ frontend/src/app/models/videojuego-model.ts
@@ -0,0 +1,9 @@
+export interface VideojuegoModel {
+    id: number;
+    titulo: string;
+    sinopsis: string;
+    genero: string;
+    multijugador: boolean;
+    lanzamiento: number;
+    desarrollador: string;
+}
