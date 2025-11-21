05e6107ae674e1fcd433367fb6b727c2e4bfe375
KevPF98 <kevinsk8.1998@gmail.com>
2025-11-21 04:19:15 -0300

    Agrego modelo Detalle Alquiler

--- frontend/src/app/models/inventario-model.ts
+++
@@ -1,9 +0,0 @@
-import { VideojuegoModel } from "./videojuego-model";
-
-export interface Inventario {
-  id: number;
-  precioDiario: number;
-  enLocal: boolean;
-  stockTotal: number;
-  videojuego: VideojuegoModel; // <--- asociación
-}
