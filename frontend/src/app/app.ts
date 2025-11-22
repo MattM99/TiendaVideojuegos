71c1e94c4ea3375e4bafb5bfbb7a6be2b207f2a4
Matias <matiasfmendoza21@gmail.com>
2025-11-22 05:17:26 -0300

    -Faltaron archivos en el commit anterior

--- frontend/src/app/app.ts
+++ frontend/src/app/app.ts
@@ -1,10 +1,12 @@
 import { HttpClient } from '@angular/common/http';
 import { Component, inject, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Header } from "./components/header/header";
+import { Footer } from "./components/footer/footer";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, Header, Footer],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
