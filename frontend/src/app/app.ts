c583d21a08dad36537bd6c779da2413d9dd55c00
Matias <matiasfmendoza21@gmail.com>
2025-11-22 19:30:03 -0300

    Modificaciones en organizacion y redacción del README.md

--- frontend/src/app/app.ts
+++ frontend/src/app/app.ts
@@ -1,8 +1,8 @@
 import { HttpClient } from '@angular/common/http';
 import { Component, inject, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { Header } from "./components/header/header";
-import { Footer } from "./components/footer/footer";
+import { Header } from "./shared/header/header";
+import { Footer } from "./shared/footer/footer";
 
 @Component({
   selector: 'app-root',
