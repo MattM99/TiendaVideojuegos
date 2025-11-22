a9cd85ee55f737af965c8724157bfacedaa9b89c
Matias <matiasfmendoza21@gmail.com>
2025-11-22 05:17:26 -0300

    -Faltaron archivos en el commit anterior

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,3 +1,16 @@
 import { Routes } from '@angular/router';
+import { Admin } from './components/admin/admin';
+import { AuthGuard } from './guards/auth-guard';
+import { Login } from './auth/login/login';
+import { FrontPage } from './components/front-page/front-page';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+ {path: 'login', component: Login },
+  { path: 'home', component: FrontPage, canActivate: [AuthGuard] },
+  { path: 'admin', component: Admin, canActivate: [AuthGuard] },
+  { path: '', redirectTo: 'login', pathMatch: 'full' },
+  { path: '**', redirectTo: 'login' }
+
+
+
+];
