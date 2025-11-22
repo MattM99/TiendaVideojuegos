f8caa696c75e77b6f0252d4516e1029a6c0d0225
Matias <matiasfmendoza21@gmail.com>
2025-11-22 05:14:25 -0300

    -Se creó la frontpage con header y footer

---
+++ frontend/src/app/components/front-page/front-page.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-front-page',
+  standalone: true,
+  imports: [],
+  templateUrl: './front-page.html',
+  styleUrl: './front-page.css',
+})
+export class FrontPage {
+
+}
