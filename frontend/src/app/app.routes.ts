28bcac812ea35f0e97d7ad5a4af2c33622ad6e4c
Pettinelli Nicolas <niadpe@gmail.com>
2025-11-23 00:28:53 -0300

    CRUD de Persona completado

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,3 +1,28 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+    path: '',
+    pathMatch: 'full',
+    redirectTo: 'personas'
+  },
+  {
+    path: 'personas',
+    loadComponent: () =>
+      import('./pages/personas-list/personas-list')
+        .then(m => m.PersonasList)
+  },
+  {
+    path: 'personas/nueva',
+    loadComponent: () =>
+      import('./pages/personas-form/personas-form')
+        .then(m => m.PersonasForm)
+  },
+  {
+    path: 'personas/:id',
+    loadComponent: () =>
+      import('./pages/personas-form/personas-form')
+        .then(m => m.PersonasForm)
+  },
+  { path: '**', redirectTo: 'personas' }
+];
--- frontend/src/app/app.ts
+++ frontend/src/app/app.ts
@@ -1,10 +1,13 @@
 import { HttpClient } from '@angular/common/http';
 import { Component, inject, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Footer } from './components/footer/footer';
+import { Header } from './components/header/header';
+
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,Footer,Header],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
