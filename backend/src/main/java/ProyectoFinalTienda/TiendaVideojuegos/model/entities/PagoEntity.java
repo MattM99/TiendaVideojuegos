5d0a2ba0b6ab2caba68db1319652ec1149a288e6
KevPF98 <kevinsk8.1998@gmail.com>
2026-04-29 09:33:54 -0300

    - Calculo automatico de costo total para instanciar pagos

--- backend/src/main/java/ProyectoFinalTienda/TiendaVideojuegos/model/entities/PagoEntity.java
+++ backend/src/main/java/ProyectoFinalTienda/TiendaVideojuegos/model/entities/PagoEntity.java
@@ -9,12 +9,12 @@ import jakarta.validation.constraints.NotNull;
 import lombok.*;
 
 import java.math.BigDecimal;
+import java.math.RoundingMode;
 
 @Entity
 @Getter
 @Setter
 @NoArgsConstructor
-@AllArgsConstructor
 @Builder
 @ToString
 @Table(
@@ -22,6 +22,15 @@ import java.math.BigDecimal;
 )
 
 public class PagoEntity {
+
+    private PagoEntity(int pagoId, EstadoPago estadoPago, BigDecimal descuento, BigDecimal penalizacionTotal, BigDecimal costoTotal) {
+        this.pagoId = pagoId;
+        this.estadoPago = estadoPago;
+        this.descuento = descuento;
+        this.penalizacionTotal = penalizacionTotal;
+        this.costoTotal = costoTotal;
+    }
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(
@@ -42,5 +51,41 @@ public class PagoEntity {
     @DecimalMin("0.0")
     @DecimalMax("1.0")
     @Digits(integer = 1, fraction = 4)
+    @Column(nullable = false, precision = 5, scale = 4)
     private BigDecimal descuento;
+
+    @NotNull
+    @DecimalMin("0.0")
+    @Digits(integer = 10, fraction = 2)
+    @Column(nullable = false, precision = 12, scale = 2)
+    private BigDecimal penalizacionTotal;
+
+    @NotNull
+    @DecimalMin("0.01")
+    @Digits(integer = 10, fraction = 2)
+    @Column(nullable = false, precision = 12, scale = 2)
+    @Setter(AccessLevel.NONE)
+    private BigDecimal costoTotal;
+
+    public static PagoEntity crear(
+            EstadoPago estadoPago,
+            BigDecimal descuento,
+            BigDecimal penalizacionTotal,
+            BigDecimal precioBase
+    ) {
+        BigDecimal descuentoAplicado = precioBase.multiply(descuento);
+
+        BigDecimal total = precioBase
+                .subtract(descuentoAplicado)
+                .add(penalizacionTotal)
+                .setScale(2, RoundingMode.HALF_UP);
+
+        return PagoEntity.builder()
+                .estadoPago(estadoPago)
+                .descuento(descuento)
+                .penalizacionTotal(penalizacionTotal)
+                .costoTotal(total)
+                .build();
+    }
+
 }
