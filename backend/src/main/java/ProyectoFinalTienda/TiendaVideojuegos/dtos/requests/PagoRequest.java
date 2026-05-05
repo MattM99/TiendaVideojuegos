4cb08637b11d666f437efcaee91d0d9833478c4f
KevPF98 <kevinsk8.1998@gmail.com>
2026-05-05 17:59:52 -0300

    - Añadido Pago Request

--- backend/src/main/java/ProyectoFinalTienda/TiendaVideojuegos/dtos/requests/PagoRequest.java
+++ backend/src/main/java/ProyectoFinalTienda/TiendaVideojuegos/dtos/requests/PagoRequest.java
@@ -1,4 +1,9 @@
 package ProyectoFinalTienda.TiendaVideojuegos.dtos.requests;
+import jakarta.validation.constraints.NotNull;
 
 public class PagoRequest {
+
+    @NotNull(message = "El ID del alquiler es obligatorio")
+    private int alquilerId;
+
 }
