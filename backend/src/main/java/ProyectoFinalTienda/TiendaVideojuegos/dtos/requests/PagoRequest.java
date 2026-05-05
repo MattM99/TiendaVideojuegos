04826ef40c08ee0861463d55ecdec0cd0046eec6
KevPF98 <kevinsk8.1998@gmail.com>
2026-05-05 20:47:33 -0300

    - Añadido Pago Service

--- backend/src/main/java/ProyectoFinalTienda/TiendaVideojuegos/dtos/requests/PagoRequest.java
+++ backend/src/main/java/ProyectoFinalTienda/TiendaVideojuegos/dtos/requests/PagoRequest.java
@@ -1,6 +1,12 @@
 package ProyectoFinalTienda.TiendaVideojuegos.dtos.requests;
 import jakarta.validation.constraints.NotNull;
+import lombok.*;
 
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class PagoRequest {
 
     @NotNull(message = "El ID del alquiler es obligatorio")
--- backend/src/main/java/ProyectoFinalTienda/TiendaVideojuegos/services/PagoService.java
+++ backend/src/main/java/ProyectoFinalTienda/TiendaVideojuegos/services/PagoService.java
@@ -1,10 +1,35 @@
 package ProyectoFinalTienda.TiendaVideojuegos.services;
 
+import ProyectoFinalTienda.TiendaVideojuegos.dtos.requests.PagoRequest;
+import ProyectoFinalTienda.TiendaVideojuegos.dtos.responses.PagoResponse;
+import ProyectoFinalTienda.TiendaVideojuegos.mappers.PagoMapper;
+import ProyectoFinalTienda.TiendaVideojuegos.model.entities.AlquilerEntity;
+import ProyectoFinalTienda.TiendaVideojuegos.model.entities.PagoEntity;
+import ProyectoFinalTienda.TiendaVideojuegos.repositories.AlquilerRepository;
 import ProyectoFinalTienda.TiendaVideojuegos.repositories.PagoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+
+import java.math.BigDecimal;
 
 public class PagoService {
     @Autowired
     private PagoRepository pagoRepository;
+    @Autowired
+    private AlquilerRepository alquilerRepository;
+    @Autowired
+    private PagoMapper pagoMapper;
+
+//    public PagoResponse crearPago(PagoRequest request) {
+//        AlquilerEntity alquiler = alquilerRepository.findById(request.getAlquilerId())
+//                .orElseThrow(() -> new RuntimeException("Alquiler no encontrado"));
+//
+//        BigDecimal precioBase = alquiler.getMontoDiarioAlquiler();
+//        BigDecimal descuento = pagoCalculator.calcularDescuento(alquiler);
+//        BigDecimal penalizacion = pagoCalculator.calcularPenalizacion(alquiler);
+//
+//        PagoEntity pago = PagoEntity.crear(alquiler, descuento, penalizacion, precioBase);
+//
+//        return pagoMapper.toResponse(pago);
+//    }
 
 }
