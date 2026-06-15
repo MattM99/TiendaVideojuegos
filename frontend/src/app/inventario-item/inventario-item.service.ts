e4a700335b0e4550ec1522277cdebcab27c3f18b
Pettinelli Nicolas <niadpe@gmail.com>
2026-06-14 22:38:25 -0300

    filtros de busqueda y arreglo de inventario en el backend pasando string a enum la plataforma

--- frontend/src/app/inventario-item/inventario-item.service.ts
+++ frontend/src/app/inventario-item/inventario-item.service.ts
@@ -5,6 +5,14 @@ import { Observable, switchMap, throwError } from 'rxjs';
 import { catchError, map } from 'rxjs/operators';
 import { VideojuegoService } from '../videojuego/videojuego.service';
 
+export interface PageResponse<T> {
+  content: T[];
+  totalPages: number;
+  totalElements: number;
+  number: number;
+  size: number;
+}
+
 @Injectable({
   providedIn: 'root',
 })
@@ -13,10 +21,82 @@ export class InventarioItemService {
   private baseUrl = 'http://localhost:8080/api/inventario';
   private videojuegoService = inject(VideojuegoService);
 
+  // Para componentes que necesitan lista simple
   getAll(): Observable<InventarioItemModel[]> {
     return this.http
       .get<{ content: InventarioItemModel[] }>(`${this.baseUrl}/listar`)
       .pipe(map(response => response.content));
+  }
+
+  // Para listado con paginación
+  getPage(
+    pagina: number,
+    tamano: number,
+    ordenarPor: string,
+    direccion: string
+  ): Observable<PageResponse<InventarioItemModel>> {
+    return this.http.get<PageResponse<InventarioItemModel>>(
+      `${this.baseUrl}/listar?pagina=${pagina}&tamano=${tamano}&ordenarPor=${ordenarPor}&direccion=${direccion}`
+    );
+  }
+
+  buscarPorVideojuego(
+    videojuegoId: number,
+    pagina: number,
+    tamano: number,
+    ordenarPor: string,
+    direccion: string
+  ): Observable<PageResponse<InventarioItemModel>> {
+    return this.http.get<PageResponse<InventarioItemModel>>(
+      `${this.baseUrl}/videojuego/${videojuegoId}?pagina=${pagina}&tamano=${tamano}&ordenarPor=${ordenarPor}&direccion=${direccion}`
+    );
+  }
+
+  buscarPorPlataforma(
+    plataforma: string,
+    pagina: number,
+    tamano: number,
+    ordenarPor: string,
+    direccion: string
+  ): Observable<PageResponse<InventarioItemModel>> {
+    return this.http.get<PageResponse<InventarioItemModel>>(
+      `${this.baseUrl}/plataforma/${plataforma}?pagina=${pagina}&tamano=${tamano}&ordenarPor=${ordenarPor}&direccion=${direccion}`
+    );
+  }
+
+  buscarMasBaratosQue(
+    valor: number,
+    pagina: number,
+    tamano: number,
+    ordenarPor: string,
+    direccion: string
+  ): Observable<PageResponse<InventarioItemModel>> {
+    return this.http.get<PageResponse<InventarioItemModel>>(
+      `${this.baseUrl}/precio/menor-a?valor=${valor}&pagina=${pagina}&tamano=${tamano}&ordenarPor=${ordenarPor}&direccion=${direccion}`
+    );
+  }
+
+  buscarPorPlataformaMasBaratosQue(
+    plataforma: string,
+    valor: number,
+    pagina: number,
+    tamano: number,
+    ordenarPor: string,
+    direccion: string
+  ): Observable<PageResponse<InventarioItemModel>> {
+    return this.http.get<PageResponse<InventarioItemModel>>(
+      `${this.baseUrl}/plataformaPrecio/menor-a`,
+      {
+        params: {
+          plataforma: plataforma,
+          valor: valor,
+          pagina: pagina,
+          tamano: tamano,
+          ordenarPor: ordenarPor,
+          direccion: direccion
+        }
+      }
+    );
   }
 
   getById(id: string | number): Observable<InventarioItemModel> {
